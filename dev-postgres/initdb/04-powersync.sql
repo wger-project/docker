5771dab1757863865dd38eb774b6ce4feb52a787
Roland Geider <roland@geider.net>
2026-04-28 19:37:24 +0200

    Update powersync storage user

--- dev-postgres/initdb/04-powersync.sql
+++ dev-postgres/initdb/04-powersync.sql
@@ -2,8 +2,8 @@
 -- living inside the wger database (separate schema, no separate DB needed).
 -- https://docs.powersync.com/configuration/powersync-service/self-hosted-instances#postgres-storage
 
-CREATE USER powersync_storage_user WITH PASSWORD 'secure_password';
-CREATE SCHEMA IF NOT EXISTS powersync AUTHORIZATION powersync_storage_user;
-GRANT CONNECT ON DATABASE wger TO powersync_storage_user;
-GRANT USAGE ON SCHEMA powersync TO powersync_storage_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA powersync TO powersync_storage_user;
+CREATE USER powersync_storage WITH PASSWORD 'powersync_password';
+CREATE SCHEMA IF NOT EXISTS powersync AUTHORIZATION powersync_storage;
+GRANT CONNECT ON DATABASE wger TO powersync_storage;
+GRANT USAGE ON SCHEMA powersync TO powersync_storage;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA powersync TO powersync_storage;
